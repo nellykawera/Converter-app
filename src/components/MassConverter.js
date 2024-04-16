6b1d34f3138c42b4efd2c554588070438e2652f4
nellykawera <kaweranelly@gmail.com>
2024-04-16 11:22:51 -0700

    Added gram as a unit in Mass Converter component

--- src/components/MassConverter.js
+++ src/components/MassConverter.js
@@ -1,37 +1,33 @@
 import React, { useState } from 'react';
-import massConverterBackgroundImage from '../images/MassConverter.jpeg';
+import backgroundImage from '../images/MassConverter.jpeg';
+import slimImage from '../images/mass-white.png';
 
 function MassConverter() {
   const [inputValue, setInputValue] = useState('');
   const [fromUnit, setFromUnit] = useState('kilogram');
   const [toUnit, setToUnit] = useState('pound');
   const [convertedValue, setConvertedValue] = useState(null);
-  const [roundOutput, setRoundOutput] = useState(false);
 
   const convertMass = () => {
-    const inputValueFloat = parseFloat(inputValue);
-    if (!isNaN(inputValueFloat)) {
+    const inputMass = parseFloat(inputValue);
+    if (!isNaN(inputMass)) {
       let convertedMass;
       if (fromUnit === 'kilogram' && toUnit === 'pound') {
-        convertedMass = inputValueFloat * 2.20462;
+        convertedMass = inputMass * 2.20462;
       } else if (fromUnit === 'pound' && toUnit === 'kilogram') {
-        convertedMass = inputValueFloat / 2.20462;
+        convertedMass = inputMass / 2.20462;
       } else if (fromUnit === 'kilogram' && toUnit === 'gram') {
-        convertedMass = inputValueFloat * 1000;
+        convertedMass = inputMass * 1000;
       } else if (fromUnit === 'gram' && toUnit === 'kilogram') {
-        convertedMass = inputValueFloat / 1000;
+        convertedMass = inputMass / 1000;
       } else if (fromUnit === 'pound' && toUnit === 'gram') {
-        convertedMass = inputValueFloat * 453.592;
+        convertedMass = inputMass * 453.592;
       } else if (fromUnit === 'gram' && toUnit === 'pound') {
-        convertedMass = inputValueFloat / 453.592;
+        convertedMass = inputMass / 453.592;
       } else {
-        convertedMass = inputValueFloat;
+        convertedMass = inputMass;
       }
-      if (roundOutput) {
-        setConvertedValue(convertedMass.toFixed(2));
-      } else {
-        setConvertedValue(convertedMass.toString());
-      }
+      setConvertedValue(convertedMass.toFixed(2));
     } else {
       setConvertedValue('Invalid input');
     }
@@ -43,53 +39,52 @@ function MassConverter() {
   };
 
   return (
-    <div className="converter-container" style={{ backgroundImage: `url(${massConverterBackgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundColor: 'green', padding: '20px', borderRadius: '8px' }}>
-      <h2 style={{ fontSize: '24px', color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', marginBottom: '20px' }}>Mass Converter</h2>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
-        <span style={{ fontSize: '18px', color: 'white' }}>Mass:</span>
-        <input type="number" id="inputValue" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter mass" style={{ fontSize: '16px', width: '200px', padding: '8px', borderRadius: '4px', border: '1px solid #ccc' }} />
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <span style={{ fontSize: '18px', marginBottom: '10px', color: 'white' }}>From:</span>
-          <div>
-            <input type="radio" id="kilogram" name="fromUnit" value="kilogram" checked={fromUnit === 'kilogram'} onChange={() => setFromUnit('kilogram')} />
-            <label htmlFor="kilogram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Kilogram</label>
+    <div>
+      <img src={slimImage} alt="Slim mass icon" style={{ width: '100%', height: 'auto' }} />
+      <div className="converter-container" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundColor: 'lightgreen', padding: '20px', borderRadius: '8px' }}>
+        <h2 style={{ fontSize: '24px', color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', marginBottom: '20px' }}>Mass Converter</h2>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+          <span style={{ fontSize: '18px', color: 'white' }}>Mass:</span>
+          <input type="number" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter mass" style={{ fontSize: '16px', width: '200px', padding: '8px', borderRadius: '4px', border: '1px solid #ccc' }} />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <span style={{ fontSize: '18px', marginBottom: '10px', color: 'white' }}>From:</span>
+            <div>
+              <input type="radio" name="fromUnit" value="kilogram" checked={fromUnit === 'kilogram'} onChange={() => setFromUnit('kilogram')} />
+              <label htmlFor="kilogram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Kilogram</label>
+            </div>
+            <div>
+              <input type="radio" name="fromUnit" value="pound" checked={fromUnit === 'pound'} onChange={() => setFromUnit('pound')} />
+              <label htmlFor="pound" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Pound</label>
+            </div>
+            <div>
+              <input type="radio" name="fromUnit" value="gram" checked={fromUnit === 'gram'} onChange={() => setFromUnit('gram')} />
+              <label htmlFor="gram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Gram</label>
+            </div>
           </div>
-          <div>
-            <input type="radio" id="pound" name="fromUnit" value="pound" checked={fromUnit === 'pound'} onChange={() => setFromUnit('pound')} />
-            <label htmlFor="pound" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Pound</label>
-          </div>
-          <div>
-            <input type="radio" id="gram" name="fromUnit" value="gram" checked={fromUnit === 'gram'} onChange={() => setFromUnit('gram')} />
-            <label htmlFor="gram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Gram</label>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <span style={{ fontSize: '18px', marginBottom: '10px', color: 'white' }}>To:</span>
+            <div>
+              <input type="radio" name="toUnit" value="kilogram" checked={toUnit === 'kilogram'} onChange={() => setToUnit('kilogram')} />
+              <label htmlFor="kilogram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Kilogram</label>
+            </div>
+            <div>
+              <input type="radio" name="toUnit" value="pound" checked={toUnit === 'pound'} onChange={() => setToUnit('pound')} />
+              <label htmlFor="pound" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Pound</label>
+            </div>
+            <div>
+              <input type="radio" name="toUnit" value="gram" checked={toUnit === 'gram'} onChange={() => setToUnit('gram')} />
+              <label htmlFor="gram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Gram</label>
+            </div>
           </div>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <span style={{ fontSize: '18px', marginBottom: '10px', color: 'white' }}>To:</span>
-          <div>
-            <input type="radio" id="toKilogram" name="toUnit" value="kilogram" checked={toUnit === 'kilogram'} onChange={() => setToUnit('kilogram')} />
-            <label htmlFor="toKilogram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Kilogram</label>
-          </div>
-          <div>
-            <input type="radio" id="toPound" name="toUnit" value="pound" checked={toUnit === 'pound'} onChange={() => setToUnit('pound')} />
-            <label htmlFor="toPound" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Pound</label>
-          </div>
-          <div>
-            <input type="radio" id="toGram" name="toUnit" value="gram" checked={toUnit === 'gram'} onChange={() => setToUnit('gram')} />
-            <label htmlFor="toGram" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Gram</label>
-          </div>
+        <div style={{ marginTop: '20px' }}>
+          <button className="convert-button" onClick={convertMass} style={{ fontSize: '16px', backgroundColor: 'black', color: 'white', padding: '8px 16px', borderRadius: '4px', marginRight: '10px' }}>Convert</button>
+          <button className="clear-button" onClick={clearInput} style={{ fontSize: '16px', backgroundColor: 'red', color: 'white', padding: '8px 16px', borderRadius: '4px' }}>Clear</button>
         </div>
+        <div className="converted-result" style={{ fontSize: '18px', color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', marginTop: '20px' }}>Converted Mass: {convertedValue}</div>
       </div>
-      <div style={{ marginTop: '20px' }}>
-        <button className="convert-button" onClick={convertMass} style={{ fontSize: '16px', backgroundColor: 'black', color: 'white', padding: '8px 16px', borderRadius: '4px', marginRight: '10px' }}>Convert</button>
-        <button className="clear-button" onClick={clearInput} style={{ fontSize: '16px', backgroundColor: 'red', color: 'white', padding: '8px 16px', borderRadius: '4px' }}>Clear</button>
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', color: 'white' }}>
-        <input type="checkbox" id="roundOutput" checked={roundOutput} onChange={() => setRoundOutput(!roundOutput)} style={{ marginRight: '10px' }} />
-        <label htmlFor="roundOutput" style={{ fontSize: '16px' }}>Round Output</label>
-      </div>
-      <div className="converted-result" style={{ fontSize: '18px', color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', marginTop: '20px' }}>Converted Mass: {convertedValue}</div>
     </div>
   );
 }
--- src/components/TemperatureConverter.js
+++ src/components/TemperatureConverter.js
@@ -1,37 +1,33 @@
 import React, { useState } from 'react';
-import temperatureBackgroundImage from '../images/TemperatureConverter.jpeg';
+import backgroundImage from '../images/TemperatureConverter.avif';
+import slimImage from '../images/temperature_white.png';
 
 function TemperatureConverter() {
   const [inputValue, setInputValue] = useState('');
   const [fromUnit, setFromUnit] = useState('celsius');
   const [toUnit, setToUnit] = useState('fahrenheit');
   const [convertedValue, setConvertedValue] = useState(null);
-  const [roundOutput, setRoundOutput] = useState(false);
 
   const convertTemperature = () => {
-    const inputValueFloat = parseFloat(inputValue);
-    if (!isNaN(inputValueFloat)) {
+    const inputTemperature = parseFloat(inputValue);
+    if (!isNaN(inputTemperature)) {
       let convertedTemp;
       if (fromUnit === 'celsius' && toUnit === 'fahrenheit') {
-        convertedTemp = (inputValueFloat * 9/5) + 32;
-      } else if (fromUnit === 'celsius' && toUnit === 'kelvin') {
-        convertedTemp = inputValueFloat + 273.15;
+        convertedTemp = (inputTemperature * 9) / 5 + 32;
       } else if (fromUnit === 'fahrenheit' && toUnit === 'celsius') {
-        convertedTemp = (inputValueFloat - 32) * 5/9;
+        convertedTemp = ((inputTemperature - 32) * 5) / 9;
+      } else if (fromUnit === 'celsius' && toUnit === 'kelvin') {
+        convertedTemp = inputTemperature + 273.15;
       } else if (fromUnit === 'fahrenheit' && toUnit === 'kelvin') {
-        convertedTemp = (inputValueFloat + 459.67) * 5/9;
+        convertedTemp = ((inputTemperature - 32) * 5) / 9 + 273.15;
       } else if (fromUnit === 'kelvin' && toUnit === 'celsius') {
-        convertedTemp = inputValueFloat - 273.15;
+        convertedTemp = inputTemperature - 273.15;
       } else if (fromUnit === 'kelvin' && toUnit === 'fahrenheit') {
-        convertedTemp = (inputValueFloat * 9/5) - 459.67;
+        convertedTemp = ((inputTemperature - 273.15) * 9) / 5 + 32;
       } else {
-        convertedTemp = inputValueFloat;
+        convertedTemp = inputTemperature;
       }
-      if (roundOutput) {
-        setConvertedValue(convertedTemp.toFixed(2));
-      } else {
-        setConvertedValue(convertedTemp.toString());
-      }
+      setConvertedValue(convertedTemp.toFixed(2));
     } else {
       setConvertedValue('Invalid input');
     }
@@ -43,53 +39,52 @@ function TemperatureConverter() {
   };
 
   return (
-    <div className="converter-container" style={{ backgroundImage: `url(${temperatureBackgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundColor: 'lightblue', padding: '20px', borderRadius: '8px' }}>
-      <h2 style={{ fontSize: '24px', padding: '10px' }}>Temperature Converter</h2>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
-        <span style={{ fontSize: '18px' }}>Temperature:</span>
-        <input type="number" id="inputValue" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter temperature" style={{ fontSize: '16px', width: '200px', padding: '8px', borderRadius: '4px', border: '1px solid #ccc' }} />
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <span style={{ fontSize: '18px' }}>From:</span>
-          <div>
-            <input type="radio" id="celsius" name="fromUnit" value="celsius" checked={fromUnit === 'celsius'} onChange={() => setFromUnit('celsius')} />
-            <label htmlFor="celsius" style={{ fontSize: '16px', marginLeft: '5px' }}>Celsius</label>
+    <div>
+      <img src={slimImage} alt="Slim temperature icon" style={{ width: '100%', height: 'auto' }} />
+      <div className="converter-container" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundColor: 'lightblue', padding: '20px', borderRadius: '8px' }}>
+        <h2 style={{ fontSize: '24px', color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', marginBottom: '20px' }}>Temperature Converter</h2>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+          <span style={{ fontSize: '18px', color: 'white' }}>Temperature:</span>
+          <input type="number" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter temperature" style={{ fontSize: '16px', width: '200px', padding: '8px', borderRadius: '4px', border: '1px solid #ccc' }} />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <span style={{ fontSize: '18px', marginBottom: '10px', color: 'white' }}>From:</span>
+            <div>
+              <input type="radio" name="fromUnit" value="celsius" checked={fromUnit === 'celsius'} onChange={() => setFromUnit('celsius')} />
+              <label htmlFor="celsius" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Celsius</label>
+            </div>
+            <div>
+              <input type="radio" name="fromUnit" value="fahrenheit" checked={fromUnit === 'fahrenheit'} onChange={() => setFromUnit('fahrenheit')} />
+              <label htmlFor="fahrenheit" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Fahrenheit</label>
+            </div>
+            <div>
+              <input type="radio" name="fromUnit" value="kelvin" checked={fromUnit === 'kelvin'} onChange={() => setFromUnit('kelvin')} />
+              <label htmlFor="kelvin" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Kelvin</label>
+            </div>
           </div>
-          <div>
-            <input type="radio" id="fahrenheit" name="fromUnit" value="fahrenheit" checked={fromUnit === 'fahrenheit'} onChange={() => setFromUnit('fahrenheit')} />
-            <label htmlFor="fahrenheit" style={{ fontSize: '16px', marginLeft: '5px' }}>Fahrenheit</label>
-          </div>
-          <div>
-            <input type="radio" id="kelvin" name="fromUnit" value="kelvin" checked={fromUnit === 'kelvin'} onChange={() => setFromUnit('kelvin')} />
-            <label htmlFor="kelvin" style={{ fontSize: '16px', marginLeft: '5px' }}>Kelvin</label>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <span style={{ fontSize: '18px', marginBottom: '10px', color: 'white' }}>To:</span>
+            <div>
+              <input type="radio" name="toUnit" value="celsius" checked={toUnit === 'celsius'} onChange={() => setToUnit('celsius')} />
+              <label htmlFor="celsius" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Celsius</label>
+            </div>
+            <div>
+              <input type="radio" name="toUnit" value="fahrenheit" checked={toUnit === 'fahrenheit'} onChange={() => setToUnit('fahrenheit')} />
+              <label htmlFor="fahrenheit" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Fahrenheit</label>
+            </div>
+            <div>
+              <input type="radio" name="toUnit" value="kelvin" checked={toUnit === 'kelvin'} onChange={() => setToUnit('kelvin')} />
+              <label htmlFor="kelvin" style={{ fontSize: '16px', marginLeft: '5px', color: 'white' }}>Kelvin</label>
+            </div>
           </div>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <span style={{ fontSize: '18px' }}>To:</span>
-          <div>
-            <input type="radio" id="toCelsius" name="toUnit" value="celsius" checked={toUnit === 'celsius'} onChange={() => setToUnit('celsius')} />
-            <label htmlFor="toCelsius" style={{ fontSize: '16px', marginLeft: '5px' }}>Celsius</label>
-          </div>
-          <div>
-            <input type="radio" id="toFahrenheit" name="toUnit" value="fahrenheit" checked={toUnit === 'fahrenheit'} onChange={() => setToUnit('fahrenheit')} />
-            <label htmlFor="toFahrenheit" style={{ fontSize: '16px', marginLeft: '5px' }}>Fahrenheit</label>
-          </div>
-          <div>
-            <input type="radio" id="toKelvin" name="toUnit" value="kelvin" checked={toUnit === 'kelvin'} onChange={() => setToUnit('kelvin')} />
-            <label htmlFor="toKelvin" style={{ fontSize: '16px', marginLeft: '5px' }}>Kelvin</label>
-          </div>
+        <div style={{ marginTop: '20px' }}>
+          <button className="convert-button" onClick={convertTemperature} style={{ fontSize: '16px', backgroundColor: 'black', color: 'white', padding: '8px 16px', borderRadius: '4px', marginRight: '10px' }}>Convert</button>
+          <button className="clear-button" onClick={clearInput} style={{ fontSize: '16px', backgroundColor: 'red', color: 'white', padding: '8px 16px', borderRadius: '4px' }}>Clear</button>
         </div>
+        <div className="converted-result" style={{ fontSize: '18px', color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', marginTop: '20px' }}>Converted Temperature: {convertedValue}</div>
       </div>
-      <div style={{ marginTop: '20px' }}>
-        <button className="convert-button" onClick={convertTemperature} style={{ fontSize: '16px', padding: '8px 16px', borderRadius: '4px', marginTop: '10px', backgroundColor: 'black', color: 'white' }}>Convert</button>
-        <button className="clear-button" onClick={clearInput} style={{ fontSize: '16px', padding: '8px 16px', borderRadius: '4px', marginTop: '10px', backgroundColor: 'red', color: 'white' }}>Clear</button>
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-        <input type="checkbox" id="roundOutput" checked={roundOutput} onChange={() => setRoundOutput(!roundOutput)} style={{ marginRight: '10px' }} />
-        <label htmlFor="roundOutput" style={{ fontSize: '16px' }}>Round Output</label>
-      </div>
-      <div className="converted-result" style={{ fontSize: '18px', padding: '10px', marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>Converted Temperature: {convertedValue}</div>
     </div>
   );
 }

ac3187a7282fe4287159d67fc487e8e01b013650
Bienvenu Murenzi <bmurenzi25@gmail.com>
2024-04-17 00:10:43 +0200

    add pages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 // App.js
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import DistanceConverter from './components/DistanceConverter';
 import TemperatureConverter from './components/TemperatureConverter';
 import MassConverter from './components/MassConverter';
+import Navbar from './components/reusable/navbar';
+import './App.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('distance');
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
+  const routes = [
+    { path: '/', element: <DistanceConverter /> },
+    { path: '/mass', element: <MassConverter /> },
+    { path: '/temperature', element: <TemperatureConverter /> }
+  ]
 
   return (
-    <div className="App">
-      <nav className="navbar">
-        <ul>
-          <li className={currentPage === 'distance' ? 'active' : ''}><button onClick={() => handlePageChange('distance')}>Distance Converter</button></li>
-          <li className={currentPage === 'temperature' ? 'active' : ''}><button onClick={() => handlePageChange('temperature')}>Temperature Converter</button></li>
-          <li className={currentPage === 'mass' ? 'active' : ''}><button onClick={() => handlePageChange('mass')}>Mass Converter</button></li>
-        </ul>
-      </nav>
-
-      <div className="content-container">
-        {currentPage === 'distance' && <DistanceConverter />}
-        {currentPage === 'temperature' && <TemperatureConverter />}
-        {currentPage === 'mass' && <MassConverter />}
+    <main>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <Routes>
+            {routes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </Router>
       </div>
-    </div>
+      
+    </main>
   );
 }
 
---
+++ src/components/reusable/navbar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><Link to='/'>Distance Converter</Link></li>
+                <li><Link to='/temperature'>Temperature Converter</Link></li>
+                <li><Link to='/mass'>Mass Converter</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
